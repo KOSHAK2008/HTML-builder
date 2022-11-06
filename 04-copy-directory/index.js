87541a281b1fb8beaed200003f426072fe638e70
KOSHAK2008 <antonkarko33@gmail.com>
2022-11-06 15:32:54 +0300

    feat: done task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,45 @@
+const fs = require('fs');
+const path = require('path');
+
+const pathFolder = path.join(__dirname, 'files');
+const pathNewFolder = path.join(__dirname, 'files-copy');
+
+fs.mkdir(pathNewFolder, { recursive: true }, (err) => {
+  if (err) console.error(err);
+  console.log('created folder copy-files');
+});
+
+fs.readdir(pathNewFolder, (err, newFiles) => {
+  if (err) console.log(err);
+  else {
+    newFiles.forEach((file) => {
+      fs.unlink(path.join(__dirname, 'files-copy', file), err => {
+        if (err) throw err;
+        // console.log(file);
+      })
+    });
+  }
+});
+
+fs.readdir(pathFolder, (err, files) => {
+  if (err) console.log(err);
+  else {
+    files.forEach((file) => {
+      // console.log(file);
+    });
+  }
+});
+
+fs.readdir(pathFolder, (err, files) => {
+  if (err) console.log(err);
+  else {
+    files.forEach((file) => {
+      fs.copyFile(path.join(__dirname, 'files', file),
+        path.join(__dirname, 'files-copy', file), (err) => {
+          if (err) console.log('Error Found:', err);
+          // console.log(file);
+        })
+    });
+  }
+});
 
