48250d3ddcace025bda75c0d25f7a5ae7c8aa6c3
KOSHAK2008 <antonkarko33@gmail.com>
2022-11-09 10:35:29 +0300

    fix: change kb from bite

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -16,7 +16,7 @@ fs.readdir(pathFile,
               console.log(err);
             else {
               console.log((file.name.slice(0, -path.extname(file.name).length) + '-'
-                + file.name.substr(file.name.indexOf('.') + 1) + '-' + (stats.size / 1000) + 'kb'));
+                + file.name.substr(file.name.indexOf('.') + 1) + '-' + (stats.size / 1024) + 'kb'));
             }
           });
         }
